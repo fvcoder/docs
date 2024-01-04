25e3d6bf72392383b004c9371d41e48dd8df0fc7
Fernando Ticona <46249834+fvcoder@users.noreply.github.com>
2024-01-03 22:24:22 -0400

    add domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.fvcoder.com',
 	integrations: [
 		starlight({
 			title: 'fvcoder docs',
